625363fca37affcdeba762c9c262235c5cdf2176
Chumachos <carloshormigo35@gmail.com>
2018-12-11 15:01:20 +0100

    Interpolation/Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { SuccessAlertComponent } from './SuccessAlert/SuccessAlert.component';
-import { WarningAlertComponent } from './WarningAlert/WarningAlert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent,
-    SuccessAlertComponent,
-    WarningAlertComponent
+    ServersComponent
   ],
   imports: [
     BrowserModule
